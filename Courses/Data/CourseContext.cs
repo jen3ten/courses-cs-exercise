0dcfe39e8497f958a42b8df7e25dc74d3c7e506d
Jen E <jen@wecancodeit.org>
2019-06-14 09:16:15 -0400

    added courses dbcontext

---
+++ Courses/Data/CourseContext.cs
@@ -0,0 +1,33 @@
+﻿using Courses.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses.Data
+{
+    public class CourseContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasData(
+                new Course()
+                {
+                    Id = 1,
+                    Name = "C# for Smarties",
+                    Description = "All you need to know about C#"
+                },
+
+                new Course()
+                {
+                    Id = 2,
+                    Name = "HTML, CSS, JS, oh my!",
+                    Description = "OMG you'll learn all about the front end"
+                }
+                );
+        }
+    }
+}
