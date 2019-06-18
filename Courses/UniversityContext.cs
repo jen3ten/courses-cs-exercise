9af692da176c7a38866781ba97cc98bfad09b001
Jen E <jen@wecancodeit.org>
2019-06-18 17:19:26 -0400

    added EF packages; added UniversityContext

---
+++ Courses/UniversityContext.cs
@@ -0,0 +1,25 @@
+﻿using Courses.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courses
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=JEUniversityDB;Trusted_Connection=True;";
+
+            optionsBuilder.UseSqlServer(connectionString)
+                          .UseLazyLoadingProxies();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
