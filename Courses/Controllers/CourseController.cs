b60f930cd0dece28a7aaeb8d0489d8b5b45dac38
Jen E <jen@wecancodeit.org>
2019-06-24 13:13:25 -0400

    added post create action to course; updated create view

--- Courses/Controllers/CourseController.cs
+++ Courses/Controllers/CourseController.cs
@@ -35,5 +35,13 @@ namespace Courses.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            course.InstructorId = 1;
+            courseRepo.Create(course);
+            return RedirectToAction("Index");
+        }
+
     }
 }
