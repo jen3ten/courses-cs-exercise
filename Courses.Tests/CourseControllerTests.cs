86338593f64a5f77da5ca76ff22a4004c9632dab
Jen E <jen@wecancodeit.org>
2019-06-26 10:38:56 -0400

    added NSubstitute dependency and using statement to test project

--- Courses.Tests/CourseControllerTests.cs
+++ Courses.Tests/CourseControllerTests.cs
@@ -5,34 +5,35 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using Xunit;
+using NSubstitute;
 
 namespace Courses.Tests
 {
     public class CourseControllerTests
     {
-        //CourseController underTest;
-        //IRepository<Course> courseRepo;
+        CourseController underTest;
+        IRepository<Course> courseRepo;
 
-        //public CourseControllerTests()
-        //{
-        //    underTest = new CourseController(courseRepo);
-        //}
+        public CourseControllerTests()
+        {
+            underTest = new CourseController(courseRepo);
+        }
 
-        //[Fact]
-        //public void Index_Returns_A_View()
-        //{
-        //    var result = underTest.Index();
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var result = underTest.Index();
 
-        //    Assert.IsType<ViewResult>(result);
-        //}
+            Assert.IsType<ViewResult>(result);
+        }
 
-        //[Fact]
-        //public void Index_Passes_CourseModel_To_View()
-        //{
-        //    var result = underTest.Index();
+        [Fact]
+        public void Index_Passes_CourseModel_To_View()
+        {
+            var result = underTest.Index();
 
-        //    Assert.IsType<List<Course>>(result.Model);
-        //}
+            Assert.IsType<List<Course>>(result.Model);
+        }
 
     }
 }
