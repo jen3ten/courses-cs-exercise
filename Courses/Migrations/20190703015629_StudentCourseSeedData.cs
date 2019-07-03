d067169b5668861f959b0d921375709381150b25
Jennifer Evans <jenevans3ten@gmail.com>
2019-07-02 22:04:57 -0400

    migration and update database; fix list and student name in course details view

---
+++ Courses/Migrations/20190703015629_StudentCourseSeedData.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Courses.Migrations
+{
+    public partial class StudentCourseSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourse_Courses_CourseId",
+                table: "StudentCourse");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourse_Student_StudentId",
+                table: "StudentCourse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourse",
+                table: "StudentCourse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Student",
+                table: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "StudentCourse",
+                newName: "StudentCourses");
+
+            migrationBuilder.RenameTable(
+                name: "Student",
+                newName: "Students");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourse_StudentId",
+                table: "StudentCourses",
+                newName: "IX_StudentCourses_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourse_CourseId",
+                table: "StudentCourses",
+                newName: "IX_StudentCourses_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Ferris Bueller" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Cameron Frye" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Sloane Peterson" });
+
+            migrationBuilder.InsertData(
+                table: "StudentCourses",
+                columns: new[] { "Id", "CourseId", "StudentId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 5, 3, 1 },
+                    { 2, 1, 2 },
+                    { 3, 2, 2 },
+                    { 4, 2, 3 },
+                    { 6, 3, 3 }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Courses_CourseId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentCourses_Students_StudentId",
+                table: "StudentCourses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StudentCourses",
+                table: "StudentCourses");
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "StudentCourses",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "StudentCourses",
+                newName: "StudentCourse");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourses_StudentId",
+                table: "StudentCourse",
+                newName: "IX_StudentCourse_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudentCourses_CourseId",
+                table: "StudentCourse",
+                newName: "IX_StudentCourse_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StudentCourse",
+                table: "StudentCourse",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourse_Courses_CourseId",
+                table: "StudentCourse",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentCourse_Student_StudentId",
+                table: "StudentCourse",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
