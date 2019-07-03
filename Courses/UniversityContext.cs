96679d5dc382c0c14dd44e0d1f8fa73e19a568a5
Jennifer Evans <jenevans3ten@gmail.com>
2019-07-02 21:54:50 -0400

    added student and studentcourse seed data

--- Courses/UniversityContext.cs
+++ Courses/UniversityContext.cs
@@ -11,6 +11,8 @@ namespace Courses
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -53,14 +55,77 @@ namespace Courses
                 new Instructor()
                 {
                     Id = 1,
-                    Name = "Jen",
+                    Name = "Jen"
                 },
 
                 new Instructor()
                 {
                     Id = 2,
-                    Name = "Ernest",
+                    Name = "Ernest"
                 });
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student()
+                {
+                    Id = 1,
+                    Name = "Ferris Bueller"
+                },
+
+                new Student()
+                {
+                    Id = 2,
+                    Name = "Cameron Frye"
+                }, 
+
+                new Student()
+                {
+                    Id = 3,
+                    Name = "Sloane Peterson" 
+                });
+
+            modelBuilder.Entity<StudentCourse>().HasData(
+                new StudentCourse()
+                {
+                    Id = 1,
+                    CourseId = 1, 
+                    StudentId = 1
+                },
+
+                new StudentCourse()
+                {
+                    Id = 2,
+                    CourseId = 1,
+                    StudentId = 2
+                },
+
+                new StudentCourse()
+                {
+                    Id = 3,
+                    CourseId = 2,
+                    StudentId = 2
+                },
+
+                new StudentCourse()
+                {
+                    Id = 4,
+                    CourseId = 2,
+                    StudentId = 3
+                },
+
+                new StudentCourse()
+                {
+                    Id = 5,
+                    CourseId = 3,
+                    StudentId = 1
+                },
+
+                new StudentCourse()
+                {
+                    Id = 6,
+                    CourseId = 3,
+                    StudentId = 3
+                });
+
 
             base.OnModelCreating(modelBuilder);
         }
